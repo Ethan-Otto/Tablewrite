71b0be573e6390adc8f7f7c9cbf7cbccf7d12fd4
Ethan Otto <ewo23@cornell.edu>
2025-10-24 00:07:33 -0400

    feat: implement scene location identification with Gemini

--- src/scene_extraction/__init__.py
+++ src/scene_extraction/__init__.py
@@ -2,5 +2,6 @@
 
 from .models import Scene, ChapterContext
 from .extract_context import extract_chapter_context
+from .identify_scenes import identify_scene_locations
 
-__all__ = ['Scene', 'ChapterContext', 'extract_chapter_context']
+__all__ = ['Scene', 'ChapterContext', 'extract_chapter_context', 'identify_scene_locations']
